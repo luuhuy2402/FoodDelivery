ae70d97841da7f6aac32347720dce5d7908b0bd0
luuhuy2402 <xeoxeo1895@gmail.com>
2025-01-02 21:52:31 +0700

    setup react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import PlaceOrder from "./pages/PlaceOrder";
 
 function App() {
     return (
         <div className="app w-[80%] m-auto">
             <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/order" element={<PlaceOrder />} />
+            </Routes>
         </div>
     );
 }
